74b42d37e805f9902120eea886dfaf7b094c5191
ayans <ayansarkar1810@gmail.com>
2022-09-02 19:33:24 +0530

    Created app bar

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'The HIT Times',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -22,9 +23,14 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
+        appBarTheme: const AppBarTheme(
+            iconTheme: IconThemeData(color: Colors.white),
+            foregroundColor: Colors.white,
+            centerTitle: true,
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'The HIT Times'),
     );
   }
 }
