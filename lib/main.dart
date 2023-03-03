baa8f5d8e7d638bdc19862a6d6f4b72e875439f2
abhijitmaiti <avhijitmaiti0123@gmail.com>
2023-03-04 02:20:37 +0530

    notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:the_hit_times_app/homepage.dart';
-
-void main() {
+import 'package:the_hit_times_app/models/notificationservice/ocal_notification_service.dart';
+Future<void> backgroundHandler(RemoteMessage message) async {
+  print(message.data.toString());
+  // print(message.notification!.title);
+}
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(backgroundHandler);
+  LocalNotificationService.initialize();
   runApp(const MyApp());
 }
 
