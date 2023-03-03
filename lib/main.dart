9386a55229a4bf74db6a868cb04149a65f242b93
a2krocks <aakapurv@gmail.com>
2023-03-03 15:49:33 +0530

    Added a In App notification

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'The HIT Times',
