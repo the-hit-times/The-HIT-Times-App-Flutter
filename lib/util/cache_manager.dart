08f08418af882217cb2597ff5a0f3b831b1413c6
a2krocks <aakapurv@gmail.com>
2023-08-25 18:43:15 +0530

    Changed Cache loading policy

--- lib/util/cache_manager.dart
+++ lib/util/cache_manager.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:http/http.dart' as http;
 
@@ -6,28 +7,43 @@ class Http {
   @Deprecated('Use getBody instead. Characters are lost when using this method')
   /// TODO: Fix characters being lost when using this method
   static Future<http.Response> get(String url, {required Map<String, String> headers}) async {
-    var file = await DefaultCacheManager().getSingleFile(url);
-    if (await file.exists()) {
-      return http.Response(await file.readAsString(), 200);
-    } else {
+    try {
       var response = await http.get(Uri.parse(url), headers: headers);
       if (response.statusCode == 200) {
         DefaultCacheManager().putFile(url,response.bodyBytes);
       }
       return response;
+    } catch (e) {
+      debugPrint("Loading Failed: $url");
+      debugPrint("Loading Cache: $url");
     }
+
+    var file = await DefaultCacheManager().getSingleFile(url);
+    if (await file.exists()) {
+      return http.Response(await file.readAsString(), 200);
+    }
+
+    return http.Response("", 404);
   }
 
   static Future<String> getBody(String url, {required Map<String, String> headers}) async {
-    var file = await DefaultCacheManager().getSingleFile(url);
-    if (await file.exists()) {
-      return await file.readAsString();
-    } else {
+
+    try {
       var response = await http.get(Uri.parse(url), headers: headers);
       if (response.statusCode == 200) {
         DefaultCacheManager().putFile(url,response.bodyBytes);
       }
       return response.body;
+    } catch (e) {
+      debugPrint("Loading Failed: $url");
+      debugPrint("Loading Cache: $url");
     }
+
+    var file = await DefaultCacheManager().getSingleFile(url);
+    if (await file.exists()) {
+      return await file.readAsString();
+    }
+
+    return "";
   }
 }
