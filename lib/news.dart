4843308b51c35795649a943176f0ed99b9a2e471
a2krocks <aakapurv@gmail.com>
2023-09-01 14:36:46 +0530

    Reverted unnecessary tabs

--- lib/news.dart
+++ lib/news.dart
@@ -144,85 +144,87 @@ class NewsState extends State<News> {
       Expanded(
         // child : DisplayBody(author: "Ayab", body: "bidu",date: "today",)
         child: RefreshIndicator(
-            onRefresh: handelRefresh,
-            child: items.isEmpty
-                ? !loading
-                    ? Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.chrome_reader_mode,
-                                color: Colors.grey, size: 60.0),
-                            Text(
-                              "No articles found",
-                              style:
-                                  TextStyle(fontSize: 24.0, color: Colors.grey),
-                            ),
-                          ],
-                        ),
-                      )
-                    : const Center(child: const CircularProgressIndicator())
-                : ListView.builder(
-                    itemCount: items.length + 1,
-                    controller: controller,
-                    itemBuilder: (BuildContext context, int index) {
-                      if (index < items.length) {
-                        return InkWell(
-                          onTap: () {
-                            Navigator.of(context).push(PageRouteBuilder(
-                              pageBuilder:
-                                  (context, animation, secondaryAnimation) =>
-                                      DisplayPost(
-                                pIndex: index,
-                                title: items[index].title,
-                                body: items[index].body,
-                                htmlBody: items[index].htmlBody,
-                                imgUrl: items[index].link,
-                                description: items[index].description,
-                                date: items[index].createdAt,
-                                category: int.parse(items[index].dropdown),
-                              ),
-                              transitionsBuilder: (context, animation,
-                                  secondaryAnimation, child) {
-                                const begin = Offset(0.0, 1.0);
-                                const end = Offset.zero;
-                                const curve = Curves.ease;
-
-                                var tween = Tween(begin: begin, end: end)
-                                    .chain(CurveTween(curve: curve));
-
-                                return SlideTransition(
-                                  position: animation.drive(tween),
-                                  child: child,
-                                );
-                              },
-                            ));
-                          },
-                          child: CusCard(
-                            link: items[index].link,
-                            title: items[index].title,
-                            description: items[index].description,
-                            body: items[index].body,
-                            createdAt: items[index].createdAt,
-                            cImage: items[index].cImage,
-                            dropdown: items[index].dropdown,
-                            id: items[index].id,
-                            updatedAt: items[index].updatedAt,
-                            bookmarked: checkAdded(items[index]),
-                            add: addnews,
-                            delete: deletenews,
+          onRefresh: handelRefresh,
+          child: items.isEmpty 
+              ? !loading ? 
+              Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.chrome_reader_mode,
+                              color: Colors.grey, size: 60.0),
+                          Text(
+                            "No articles found",
+                            style:
+                                TextStyle(fontSize: 24.0, color: Colors.grey),
                           ),
-                        );
-                      } else {
-                        return hasmore
-                            ? const Padding(
-                                padding: EdgeInsets.symmetric(vertical: 30),
-                                child: const Center(
-                                    child: CircularProgressIndicator()))
-                            : Container();
-                      }
-                    },
-                  )),
+                        ],
+                      ),
+                    )
+                    : const Center(child: const CircularProgressIndicator())
+              : 
+                   ListView.builder(
+                      itemCount: items.length + 1,
+                      controller: controller,
+                      itemBuilder: (BuildContext context, int index) {
+                        if (index < items.length) {
+                          return InkWell(
+                            onTap: () {
+                              Navigator.of(context).push(PageRouteBuilder(
+                                pageBuilder:
+                                    (context, animation, secondaryAnimation) =>
+                                        DisplayPost(
+                                  pIndex: index,
+                                  title: items[index].title,
+                                  body: items[index].body,
+                                  htmlBody: items[index].htmlBody,
+                                  imgUrl: items[index].link,
+                                  description: items[index].description,
+                                  date: items[index].createdAt,
+                                  category: int.parse(items[index].dropdown),
+                                ),
+                                transitionsBuilder: (context, animation,
+                                    secondaryAnimation, child) {
+                                  const begin = Offset(0.0, 1.0);
+                                  const end = Offset.zero;
+                                  const curve = Curves.ease;
+
+                                  var tween = Tween(begin: begin, end: end)
+                                      .chain(CurveTween(curve: curve));
+
+                                  return SlideTransition(
+                                    position: animation.drive(tween),
+                                    child: child,
+                                  );
+                                },
+                              ));
+                            },
+                            child: CusCard(
+                              link: items[index].link,
+                              title: items[index].title,
+                              description: items[index].description,
+                              body: items[index].body,
+                              createdAt: items[index].createdAt,
+                              cImage: items[index].cImage,
+                              dropdown: items[index].dropdown,
+                              id: items[index].id,
+                              updatedAt: items[index].updatedAt,
+                              bookmarked: checkAdded(items[index]),
+                              add: addnews,
+                              delete: deletenews,
+                            ),
+                          );
+                        } else {
+                          return hasmore
+                              ? const Padding(
+                                  padding: EdgeInsets.symmetric(vertical: 30),
+                                  child: const Center(
+                                      child: CircularProgressIndicator()))
+                              : Container();
+                        }
+                      },
+                    )
+        ),
       ),
     ]);
   }
